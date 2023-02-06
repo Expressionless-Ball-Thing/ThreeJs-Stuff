e75ef6f0da5f8bf9738c7a57f70798276728e7b5
Expressionless Ball Thing <lowestley0409@gmail.com>
2023-02-06 14:12:50 +1100

    added physics, WASD now just strafe on the xy plane

--- pages/stuff/FlightGame.tsx
+++ pages/stuff/FlightGame.tsx
@@ -4,7 +4,7 @@ import {
   useKeyboardControls
 } from "@react-three/drei";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { CuboidCollider, Debug, Physics, RigidBody } from "@react-three/rapier";
+import { CuboidCollider, Debug, Physics, RigidBody, useRapier } from "@react-three/rapier";
 import { useRef } from "react";
 import * as THREE from "three";
 import ThreeStuffBox from "../../components/layouts/CanvasBox";
@@ -37,27 +37,27 @@ function Scene() {
     <>
     <RigidBody type="fixed">
       <mesh position={[0, 0, 12.5]}>
-        <planeGeometry args={[25, 25, 10, 10]} />
+        <boxGeometry args={[25, 25, 1, 10, 10, 10]} />
         <meshBasicMaterial color="blue" side={THREE.DoubleSide} wireframe />
       </mesh>
       <mesh position={[0, 0, -12.5]}>
-        <planeGeometry args={[25, 25, 10, 10]} />
+        <boxGeometry args={[25, 25, 1, 10, 10, 10]} />
         <meshBasicMaterial color="blue" side={THREE.DoubleSide} wireframe />
       </mesh>
       <mesh position={[0, 12.5, 0]} rotation-x={Math.PI / 2}>
-        <planeGeometry args={[25, 25, 10, 10]} />
+        <boxGeometry args={[25, 25, 1, 10, 10, 10]} />
         <meshBasicMaterial color="blue" side={THREE.DoubleSide} wireframe />
       </mesh>
       <mesh position={[0, -12.5, 0]} rotation-x={Math.PI / 2}>
-        <planeGeometry args={[25, 25, 10, 10]} />
+        <boxGeometry args={[25, 25, 1, 10, 10, 10]} />
         <meshBasicMaterial color="blue" side={THREE.DoubleSide} wireframe />
       </mesh>
       <mesh position={[12.5, 0, 0]} rotation-y={Math.PI / 2}>
-        <planeGeometry args={[25, 25, 10, 10]} />
+        <boxGeometry args={[25, 25, 1, 10, 10, 10]} />
         <meshBasicMaterial color="blue" side={THREE.DoubleSide} wireframe />
       </mesh>
       <mesh position={[-12.5, 0 , 0]} rotation-y={Math.PI / 2}>
-        <planeGeometry args={[25, 25, 10, 10]} />
+        <boxGeometry args={[25, 25, 1, 10, 10, 10]} />
         <meshBasicMaterial color="blue" side={THREE.DoubleSide} wireframe />
       </mesh>
     </RigidBody>
@@ -70,42 +70,50 @@ function Player() {
   const pointControls = useRef(null);
   const playerRef = useRef(null);
 
-  const frontVector = new THREE.Vector3();
-  const sideVector = new THREE.Vector3();
-  const verticalVector = new THREE.Vector3();
-  const direction = new THREE.Vector3();
-
-  const SPEED = 5;
-
   const [, getKeys] = useKeyboardControls();
 
-  useFrame((state) => {
+  useFrame((state, delta) => {
     const { forward, backward, leftward, rightward, up, down } = getKeys();
     state.camera.position.set(...playerRef.current.translation());
     playerRef.current!.setRotation(state.camera.quaternion);
 
-    frontVector.set(0, 0, +backward - +forward);
-    sideVector.set(+leftward - +rightward, 0, 0);
-    verticalVector.set(0, +up - +down, 0);
-    direction
-      .subVectors(frontVector.addScaledVector(verticalVector, 1), sideVector)
-      .normalize()
-      .multiplyScalar(SPEED)
-      .applyEuler(state.camera.rotation);
+    const impulseStrength = 0.1 * delta
 
+    playerRef.current.applyImpulse(state.camera.getWorldDirection(new THREE.Vector3).normalize().multiplyScalar(impulseStrength))
 
-    playerRef.current!.setLinvel({
-      x: direction.x,
-      y: direction.y,
-      z: direction.z,
-    });
+    const impulse = { x: 0, y: 0, z: 0 }
+
+    if(forward)
+    {
+        impulse.y += impulseStrength
+    }
+
+    if(rightward)
+    {
+        impulse.x += impulseStrength
+        
+    }
+
+    if(backward)
+    {
+        impulse.y -= impulseStrength
+        
+    }
+    
+    if(leftward)
+    {
+        impulse.x -= impulseStrength
+    }
+
+    playerRef.current.applyImpulse(new THREE.Vector3(impulse.x, impulse.y, impulse.z).applyEuler(state.camera.rotation))
+
   });
 
   return (
     <>
       <PointerLockControls ref={pointControls} maxPolarAngle={Math.PI * 2} />
       <Debug />
-      <RigidBody ref={playerRef} colliders={false} linearDamping={0.5}>
+      <RigidBody ref={playerRef} colliders={false} linearDamping={0.5} restitution={2}>
         <CuboidCollider args={[0.1, 0.1, 0.1]} />
       </RigidBody>
     </>
